f0f3d16e2b40dda822a489b13ccfec973a8c708c
Chandu Bhogadi <chandrahasbhogadi@gmail.com>
2023-06-09 20:50:55 +1000

    html css and js code being combined

--- public/script.js
+++ public/script.js
@@ -1,194 +1,187 @@
-// Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const tasklist = document.getElementById("tasklist");
+// Get references to HTML elements
+const taskForm = document.getElementById("taskform");
+const taskList = document.getElementById("tasklist");
+const taskModal = document.getElementById("task-modal");
+const pushButton = document.getElementById("pushButton");
+const pullButton = document.getElementById("pullButton");
+const legsButton = document.getElementById("legsButton");
+const pushSection = document.getElementById("push-sec");
+const pullSection = document.getElementById("pull-sec");
+const legsSection = document.getElementById("legs-sec");
+const taskTypeSelect = document.getElementById("taskType");
 
+// Initialize tasks array
+let tasks = [];
 
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    addTask(
-        form.elements.taskName.value,
-        form.elements.taskType.value,
-        form.elements.taskRate.value,
-        form.elements.taskTime.value,
-        form.elements.taskClient.value
-    )
-})
-
-// function displayTasks() {
-
-//   tasklist.innerHTML = "";
-
-//   let localTasks = JSON.parse(localStorage.getItem('tasks'));
- 
-//   if (localTasks != null) {
-
-//     localTasks.forEach((task) =>  {
-    
-//       console.log(task)
-    
-//         // Create task items for the DOM and add to the list
-//         let item = document.createElement("li");
-//         item.setAttribute("data-id", task.id);
-//         item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
-//         tasklist.appendChild(item);
-
-//         // Clear the value of the input once the task has been added to the page
-//         form.reset();
-
-//         // Setup delete button DOM elements
-//         let delButton = document.createElement("button");
-//         let delButtonText = document.createTextNode("Delete");
-//         delButton.appendChild(delButtonText);
-//         item.appendChild(delButton); // Adds a delete button to every task
-
-//         // Listen for when the delete button is clicked
-//         delButton.addEventListener("click", function (event) {
-
-//             localTasks.forEach(function (taskArrayElement, taskArrayIndex) {
-//                 if (taskArrayElement.id == item.getAttribute('data-id')) {
-//                     localTasks.splice(taskArrayIndex, 1)
-//                 }
-//             })
-
-//             localStorage.setItem('tasks', JSON.stringify(localTasks));
-
-//             item.remove(); // Remove the task item from the page when button clicked
-//             // Because we used 'let' to define the item, 
-//             // this will always delete the right element
-//       })
-
-//     })
-// }
-// }
-
-
-
-const taskTypeImages = {
-    Chest:"./images/muscles-chest.png",
-    Shoulders: "./images/muscles-chest.png",
-    Triceps: "./images/muscles-tri.png",
-};
-
-function testImage(URL) {
-  var tester=new Image();
-  tester.onload=imageFound;
-  tester.onerror=imageNotFound;
-  tester.src=URL;
+// Check if tasks exist in local storage
+const storedTasks = localStorage.getItem("tasks");
+if (storedTasks) {
+  tasks = JSON.parse(storedTasks);
+  renderTasks();
 }
 
-function imageFound() {
-  alert('That image is found and loaded');
+// Update muscle group options based on section click
+pushSection.addEventListener("click", () => {
+  updateMuscleGroupOptions("Chest", "Shoulders", "Triceps");
+});
+
+pullSection.addEventListener("click", () => {
+  updateMuscleGroupOptions("Back", "Biceps");
+});
+
+legsSection.addEventListener("click", () => {
+  updateMuscleGroupOptions("Quads", "Hamstrings", "Calves");
+});
+
+function updateMuscleGroupOptions(...options) {
+  // Clear existing options
+  taskTypeSelect.innerHTML = "";
+
+  // Add new options
+  options.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option;
+    optionElement.textContent = option;
+    taskTypeSelect.appendChild(optionElement);
+  });
 }
 
-function imageNotFound() {
-  alert('That image was not found.');
+function renderTasks() {
+  // Clear task list
+  taskList.innerHTML = "";
+
+  // Create HTML for each task
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+
+    // Add hover effect and color change on hover
+    taskItem.addEventListener("mouseenter", () => {
+      taskItem.classList.add("hovered");
+    });
+
+    taskItem.addEventListener("mouseleave", () => {
+      taskItem.classList.remove("hovered");
+    });
+
+    const taskName = document.createElement("h4");
+    taskName.textContent = task.name;
+
+    const taskWeight = document.createElement("p");
+    taskWeight.innerHTML = `Weight: <span id="taskWeight-${index}">${task.rate}</span> ${task.metric}`;
+
+    const taskSets = document.createElement("p");
+    taskSets.textContent = `Sets: ${task.time}`;
+
+    const taskReps = document.createElement("p");
+    taskReps.textContent = `Reps: ${task.client}`;
+
+    taskItem.appendChild(taskName);
+    taskItem.appendChild(taskWeight);
+    taskItem.appendChild(taskSets);
+    taskItem.appendChild(taskReps);
+
+    // Open the modal on task item click
+    taskItem.addEventListener("click", () => showTaskDetails(index));
+
+    // Prepend the task item to the task list
+    taskList.insertAdjacentElement("afterbegin", taskItem);
+  });
 }
 
-testImage("public\images\muscles-chest.png");
 
+function showTaskDetails(index) {
+  const task = tasks[index];
+  const modalContent = `
+    <h3>${task.name}</h3>
+    <p>Muscle Group: ${task.type}</p>
+    <p>Weight: <span id="taskWeight">${task.rate}</span> ${task.metric}</p>
+    <p>Sets: ${task.time}</p>
+    <p>Reps: ${task.client}</p>
+    <h4>Notes</h4>
+    <ul>${task.notes.map((note) => `<li>${note}</li>`).join("")}</ul>
+    <form id="noteForm">
+      <label for="noteInput">Add Note:</label>
+      <input type="text" id="noteInput" required />
+      <button type="submit">Add</button>
+    </form>
+    <button id="deleteTask">Delete</button>
+    <button id="closeModal">Close</button>
+  `;
 
-function displayTasks() {
-    tasklist.innerHTML = "";
-  
-    let localTasks = JSON.parse(localStorage.getItem('tasks'));
-  
-    if (localTasks != null && localTasks.length > 0) {
-      localTasks.forEach((task) => {
-        console.log(task);
-  
-        // Create task items for the DOM and add to the list
-        let item = document.createElement("div");
-        item.setAttribute("data-id", task.id);
-  
-        // Create the image element with the corresponding task type image
-        let img = document.createElement("img");
-        img.src = taskTypeImages[task.type]; // Set the image source based on the task type
-        item.appendChild(img);
-  
-        // Create the task details container
-        let details = document.createElement("div");
-        details.innerHTML = `
-          <strong>${task.name}</strong><br>
-          <span>${task.type}</span><br>
-          <span>Weight: ${task.rate} kg</span><br>
-          <span>Reps: ${task.client}</span><br>
-          <span>Sets: ${task.time}</span>
-        `;
-        item.appendChild(details);
-  
-        tasklist.appendChild(item);
-  
-        // Clear the value of the input once the task has been added to the page
-        form.reset();
-  
-        // Setup delete button DOM elements
-        let delButton = document.createElement("button");
-        let delButtonText = document.createTextNode("Delete");
-        delButton.appendChild(delButtonText);
-        item.appendChild(delButton); // Adds a delete button to every task
-  
-        // Listen for when the delete button is clicked
-        delButton.addEventListener("click", function (event) {
-          localTasks.forEach(function (taskArrayElement, taskArrayIndex) {
-            if (taskArrayElement.id == item.getAttribute('data-id')) {
-              localTasks.splice(taskArrayIndex, 1);
-            }
-          });
-  
-          localStorage.setItem('tasks', JSON.stringify(localTasks));
-  
-          item.remove(); // Remove the task item from the page when button clicked
-          // Because we used 'let' to define the item,
-          // this will always delete the right element
-        });
-      });
-    } else {
-      tasklist.classList.add("no-tasks"); // Add CSS class to tasklist when there are no tasks
+  taskModal.innerHTML = modalContent;
+  taskModal.showModal();
+
+  // Handle note submission
+  const noteForm = document.getElementById("noteForm");
+  noteForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const noteInput = document.getElementById("noteInput");
+    const note = noteInput.value.trim();
+    if (note) {
+      task.notes.push(note);
+      showTaskDetails(index); // Re-render the modal to display the updated notes
+      noteInput.value = ""; // Clear the note input field
+      saveTasks(); // Save tasks to local storage
     }
-  }
-  
-  
-  //q: why is there a div before we add the task?
+  });
 
-  
-  
+  // Handle task deletion
+  const deleteButton = document.getElementById("deleteTask");
+  deleteButton.addEventListener("click", () => {
+    tasks.splice(index, 1); // Remove the task from the array
+    renderTasks(); // Re-render the tasks list
+    saveTasks(); // Save tasks to local storage
+    taskModal.close(); // Close the modal
+  });
 
-// Function to add task to the list
-function addTask(name, type, rate, time, client) {
-
-    // Creating the object, directly passing in the input parameters
-    let task = {
-        name,
-        type,
-        id: Date.now(),
-        date: new Date().toISOString(),
-        rate,
-        time,
-        client
-    }
-    
-    // fetching and parse localStorage value
-    let localTasks = JSON.parse(localStorage.getItem('tasks'));
-    
-    if (localTasks == null) {
-        localTasks = [task];
-    } else {
-        // Check to see if there is an existing task
-        if (localTasks.find(element => element.id === task.id)) {
-            console.log('Task ID already exists');
-        } else {
-            localTasks.push(task);
-        }
-    }
-    
-    localStorage.setItem('tasks', JSON.stringify(localTasks));
-
-    displayTasks();
-
+  // Handle modal close
+  const closeModalButton = document.getElementById("closeModal");
+  closeModalButton.addEventListener("click", () => {
+    taskModal.close();
+  });
 }
 
-// Call the function with test values for the input paramaters
-// addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
-displayTasks();
+// Function to add a new task
+function addTask(event) {
+  event.preventDefault();
+
+  // Get input values
+  const taskName = document.getElementById("taskName").value;
+  const taskType = document.getElementById("taskType").value;
+  const taskRate = document.getElementById("taskRate").value;
+  const taskTime = document.getElementById("taskTime").value;
+  const taskClient = document.getElementById("taskClient").value;
+  const taskMetric = document.getElementById("taskMetric").value;
+
+  // Create a new task object
+  const newTask = {
+    name: taskName,
+    type: taskType,
+    rate: taskRate,
+    metric: taskMetric,
+    time: taskTime,
+    client: taskClient,
+    notes: [],
+  };
+
+  // Add the new task to the tasks array
+  tasks.push(newTask);
+
+  // Render tasks
+  renderTasks();
+
+  // Clear form inputs
+  taskForm.reset();
+
+  // Save tasks to local storage
+  saveTasks();
+}
+
+// Function to save tasks to local storage
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Event listener for form submission
+taskForm.addEventListener("submit", addTask);
